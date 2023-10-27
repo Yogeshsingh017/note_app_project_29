868e0e822c7d397ab0977cdd55dc93cff9f25e59
YOGESH <142924080+Yogeshsingh017@users.noreply.github.com>
2023-10-27 20:34:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
-const num1 = Math.ceil(Math.random() * 10);
-const num2 = Math.ceil(Math.random() * 10);
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
 
-const questionEl = document.getElementById("question");
-
-const inputEl = document.getElementById("input");
-
-const formEl = document.getElementById("form");
-
-const scoreEl = document.getElementById("score");
-
-let score = JSON.parse(localStorage.getItem("score"));
-
-if (!score) {
-  score = 0;
-}
-
-scoreEl.innerText = `score: ${score}`;
-
-questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
-
-const correctAns = num1 * num2;
-
-formEl.addEventListener("submit", () => {
-  const userAns = +inputEl.value;
-  if (userAns === correctAns) {
-    score++;
-    updateLocalStorage();
-  } else {
-    score--;
-    updateLocalStorage();
-  }
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
 });
 
-function updateLocalStorage() {
-  localStorage.setItem("score", JSON.stringify(score));
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+
+  return element;
 }
+
+function deleteNote(id, element) {
+    const notes = getNotes().filter((note)=>note.id != id)
+    saveNote(notes)
+    appEl.removeChild(element)
+}
+
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
+}
+
+function addNote() {
+  const notes = getNotes();
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+
+  saveNote(notes);
+}
+
+function saveNote(notes) {
+  localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click", addNote);
